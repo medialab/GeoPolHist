d12fc28b67512757dd01cef646ceed5928f526ed
ArnaudMolo <hi@onesime.fr>
2019-08-22 17:25:33 +0200

    Updates

--- precache-manifest.2d0472a665bfc8cef5a687c4f2da2f6c.js
+++ precache-manifest.2d0472a665bfc8cef5a687c4f2da2f6c.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "541baf3e7f0d7d9ef07aceacc25e8978",
+    "revision": "46da712a9dabd8acd3915d87386ec2e2",
     "url": "/ric_entities_timelines/index.html"
   },
   {
-    "revision": "60a70777d1356e011329",
+    "revision": "c6c52b75961f97f29eae",
     "url": "/ric_entities_timelines/static/css/main.f4ea7ee8.chunk.css"
   },
   {
-    "revision": "5ee19678160bf5e64393",
-    "url": "/ric_entities_timelines/static/js/2.8ba2c040.chunk.js"
+    "revision": "633aead9ef0dddb81a2a",
+    "url": "/ric_entities_timelines/static/js/2.16dbe3af.chunk.js"
   },
   {
-    "revision": "60a70777d1356e011329",
-    "url": "/ric_entities_timelines/static/js/main.35b95a0c.chunk.js"
+    "revision": "c6c52b75961f97f29eae",
+    "url": "/ric_entities_timelines/static/js/main.b4ab38a1.chunk.js"
   },
   {
     "revision": "1a657f9bfa7f362bb373",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ric_entities_timelines/precache-manifest.9a8dd0ed8a36bcbabc15b8e53f21c25e.js"
+  "/ric_entities_timelines/precache-manifest.2d0472a665bfc8cef5a687c4f2da2f6c.js"
 );
 
 self.addEventListener('message', (event) => {
