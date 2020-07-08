d3c2dcacaf6bd553d7c5ffe6e24eee2c3eef0c0b
Paul Girard <paul@ouestware.com>
2020-07-08 15:08:00 +0200

    removed unused status + better colours thanks to @jacomyma Iwanthue

--- src/timeline.tsx
+++ src/timeline.tsx
@@ -10,7 +10,27 @@ import { translate, STATUS_SLUG } from './utils';
 import './timeline.css'
 import uuid from 'uuid';
 
-const colorScale = scaleOrdinal(['#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#808080', '#ffffff', '#000000']).domain(values(STATUS_SLUG));
+// color scale generated thanks to @jacomyma tools iwanthue
+const colorScale = scaleOrdinal(["#cd7f3d",
+"#af49d8",
+"#65da57",
+"#d84397",
+"#cbe240",
+"#6c6cd4",
+"#a3ca63",
+"#ad63ab",
+"#489046",
+"#d74433",
+"#6cdaaf",
+"#c15b67",
+"#86c5d8",
+"#d8b94f",
+"#6779a8",
+"#787938",
+"#d4a8cc",
+"#4f8178",
+"#cdcea7",
+"#997462"]).domain(values(STATUS_SLUG));
 const legend = {
   height: 50,
 }
--- src/utils.ts
+++ src/utils.ts
@@ -10,7 +10,6 @@ export const translate_link_type: any =  {
   "Became colony of": {"slug":"col","priority":3},
   "Became possession of": {"slug":"poss","priority":3},
   "Became dependency of": {"slug":"dep", "priority":3},
-  "Became concession of": {"slug":"cons", "priority":3},
   "Claimed by": {"slug":"claim","priority":1},
   "Became protectorate of": {"slug":"prot","priority":3},
   "Became associated state of": {"slug":"assoc","priority":4},
@@ -19,13 +18,10 @@ export const translate_link_type: any =  {
   "Became neutral or demilitarized zone of": {"slug":"neut","priority":1},
   "Mandated to": {"slug":"mand","priority":2},
   "Sovereign": {"slug":"SOV","priority":5},
-  "Unincorporated territory": {"slug":"uninc","priority":0},
-  "Autonomous constituent country of": {"slug":"autonom","priority":0},
   "Sovereign (unrecognized)": {"slug":"SOV_U","priority":4},
   "Sovereign (limited)": {"slug":"SOV_L","priority":4},
   "International":  {"slug":"int","priority":-1},
   "Informal":  {"slug":"inf","priority":-1},
-  "Protected area of": {"slug":"protected","priority":0},
   "Unknown": {"slug":"N/A","priority":0},
 }
 
@@ -45,15 +41,11 @@ export enum STATUS_SLUG {
   neut = "neut",
   mand = "mand",
   SOV = "SOV",
-  uninc = "uninc",
-  autonom = "autonom",
   SOV_U = "SOV_U",
   SOV_L = "SOV_L",
-  protected = "protected",
   NA = "N/A",
   alliance = "alliance",
   inf = "informal",
-  cons = "concession",
   int = "international",
   dep = "dependency"
 }
