e8b71ac511b974a5dd97fa8fab61e5f97a333651
Paul Girard <paul@ouestware.com>
2020-07-31 11:42:47 +0200

    legend interactivity corrected #5 entity name as links #7 GPH_code in infobox #12 number of controlled entities #13

--- src/Country.tsx
+++ src/Country.tsx
@@ -5,7 +5,7 @@ import equals from 'ramda/es/equals';
 import { AppContext } from './AppContext';
 import Timelines from './timeline';
 import { Link as RLink, RouteComponentProps } from 'react-router-dom';
-import { min, max, scaleTime } from 'd3';
+import { min, max, scaleTime, scaleOrdinal } from 'd3';
 
 const dateReducer = (accumulator, link: Link) => accumulator + (+link.end_year) - +link.start_year
 const sortByDuration = sort(([, aLinks], [, bLinks]) => bLinks.reduce(dateReducer, 0) - aLinks.reduce(dateReducer, 0));
@@ -17,11 +17,8 @@ const Country: React.FC<{
   id: string
 }>> = (props) => {
   const {history, country} = props;
-  const onOccupiedLinkClick = useCallback(link => {
-    history.push(`/country/${link.sovereign.id}`);
-  }, [history]);
-  const onCampainsLinkClick = useCallback(link => {
-    history.push(`/country/${link.GPH_code}`);
+  const onEntityClick = useCallback(GPH_code => {
+    history.push(`/country/${GPH_code}`);
   }, [history]);
   const occupations: [Entity, Link[]][] = Array.from(country.occupations.associations());
   const campains: [Entity, Link[]][] = Array.from(country.campains.associations());
@@ -32,32 +29,57 @@ const Country: React.FC<{
   const minDate = min(data, ([{start}]) => start);
   const maxDate = max(data, ([{end}]) => end);
   const xScale = scaleTime().domain([minDate, maxDate]);
+  // color scale generated thanks to @jacomyma tools iwanthue
+  const {state}: {state: GlobalState} = useContext(AppContext);
+  const colorScale = scaleOrdinal(["#cd7f3d",
+  "#af49d8",
+  "#65da57",
+  "#d84397",
+  "#cbe240",
+  "#6c6cd4",
+  "#a3ca63",
+  "#ad63ab",
+  "#489046",
+  "#d74433",
+  "#6cdaaf",
+  "#c15b67",
+  "#86c5d8",
+  "#d8b94f",
+  "#6779a8",
+  "#787938",
+  "#d4a8cc",
+  "#4f8178",
+  "#cdcea7",
+  "#997462"]).domain(Object.keys(state.status).map(s => state.status[s].slug));
   return (
     <div>
       <aside>
-        <RLink to='/'>Home</RLink>
+        <RLink to='/'>GeoPolHist</RLink>
       </aside>
-      <h1>{country.name} ({country.id})</h1>
+      <h1>{country.name}</h1>
+      <p>GPH_code : {country.id}</p>
       <h2>Status</h2>
       <Timelines
-        onLinkClick={onOccupiedLinkClick}
+        onEntityClick={onEntityClick}
         intervalMinWidth={5}
         data={occupations}
         nbLines={country.occupations.dimension}
         lineHeight={20}
         width={1000}
         xScale={xScale}
+        colorScale={colorScale}
       />
       {/* <Histogram width={1000} height={300} data={Array.from(country.campains.values())} /> */}
-      <h2>Sovereign of</h2>
+      <h2>Sovereign of {country.campains.dimension} entities</h2>
       <Timelines
-        onLinkClick={onCampainsLinkClick}
+        onEntityClick={onEntityClick}
         intervalMinWidth={5}
         data={campains}
         nbLines={country.campains.dimension}
         lineHeight={20}
         width={1000}
         xScale={xScale}
+        colorScale={colorScale}
       />
     </div>
   );
--- src/timeline.tsx
+++ src/timeline.tsx
@@ -1,8 +1,7 @@
 import React, { useState, useEffect, useMemo, useContext } from 'react';
-import { scaleOrdinal, axisBottom, select, scaleLinear, timeFormat, ScaleTime, axisTop } from 'd3';
+import { scaleOrdinal, axisBottom, select, scaleLinear, timeFormat, ScaleTime, axisTop, ScaleOrdinal } from 'd3';
 import values from 'ramda/es/values';
 import reduce from 'ramda/es/reduce';
-import countBy from 'ramda/es/countBy';
 import sort from 'ramda/es/sort';
 import mapObjIndexed from 'ramda/es/mapObjIndexed';
 import pipe from 'ramda/es/pipe';
@@ -10,7 +9,6 @@ import cx from 'classnames';
 import { translate } from './utils';
 import './timeline.css'
 import uuid from 'uuid';
-import { AppContext } from './AppContext';
 
 
 const legend = {
@@ -24,12 +22,6 @@ const margins = {
 };
 const formater = timeFormat('%Y');
 
-const groupByStatus = pipe(
-  reduce((acc, [, links]: [Entity, Link[]]) => [...acc, ...links], []),
-  reduce((acc, link:Link) => { 
-    acc[link.status.slug] = link.status;
-    return acc;}, {})
-);
 
 const Timelines: React.FC<{
   data: [Entity, Link[]][];
@@ -37,36 +29,14 @@ const Timelines: React.FC<{
   intervalMinWidth: number;
   lineHeight: number;
   width: number;
-  onLinkClick: (link: Link) => void;
+  onEntityClick: (GPH_code: string) => void;
   xScale: ScaleTime<number, number>;
   nbLines: number;
+  colorScale: ScaleOrdinal<string, string>;
 }> = props => {
 
-  // color scale generated thanks to @jacomyma tools iwanthue
-  const {state}: {state: GlobalState} = useContext(AppContext);
-  const colorScale = scaleOrdinal(["#cd7f3d",
-  "#af49d8",
-  "#65da57",
-  "#d84397",
-  "#cbe240",
-  "#6c6cd4",
-  "#a3ca63",
-  "#ad63ab",
-  "#489046",
-  "#d74433",
-  "#6cdaaf",
-  "#c15b67",
-  "#86c5d8",
-  "#d8b94f",
-  "#6779a8",
-  "#787938",
-  "#d4a8cc",
-  "#4f8178",
-  "#cdcea7",
-  "#997462"]).domain(Object.keys(state.status).map(s => state.status[s].slug));
-
   // useWhyDidYouUpdate('timeline', props);
-  const { xScale, data, nbLines } = props;
+  const { xScale, data, nbLines, colorScale } = props;
   const id = useMemo(uuid, []);
   const width = props.width - margins.left - margins.right;
   const elHeight = nbLines * props.lineHeight + margins.top + margins.bottom;
@@ -84,18 +54,17 @@ const Timelines: React.FC<{
   const intervalBarMargin = (groupHeight - intervalBarHeight) / 2;
   const [hover, setHover] = useState<{link: Link, index: number}>();
   const [status, setStatus] = useState<any>();
-  const groupedByStatus = useMemo(() => groupByStatus(data), [data]);
+  
+  const groupedByStatus = data.reduce((acc, [, links]: [Entity, Link[]]) => [...acc, ...links], [])
+                      .reduce((acc, link:Link) => { 
+                          acc[link.status.slug] = link.status;
+                          return acc;}, {});
   const sortDate = sort((a:Date, b:Date) => a > b)
   const sortEntityByStartDate = sort((a: [Entity, Link[]], b:[Entity, Link[]])=> sortDate(a[1].map(l => l.start_year))[0] > sortDate(b[1].map(l => l.start_year))[0])
+  console.log(data)
   useEffect(() => () => {setHover(null); setStatus(null)}, [props.data]);
   return (
     <div className='timelines-container' style={{width: width}} onMouseLeave={() => setHover(null)}>
-      {hover && <div className='tooltip-container' style={{
-        transform: `translate(${xScale(hover.link.start_year)}px, ${yScale(hover.index) + legend.height + margins.top + props.lineHeight + 20}px)`,
-        minWidth: intervalRectWidth(hover.link)
-      }}>
-        <span className='tooltip'>{hover.link.GPH_name} was a {hover.link.status.slug} from {formater(hover.link.start_year)} to {formater(hover.link.end_year)}</span>
-      </div>}
       <div className="legend">
         <div className='legend-container'>
           {values(mapObjIndexed((s, slug) => 
@@ -121,62 +90,70 @@ const Timelines: React.FC<{
           </g>
         </svg>
       </div>
-      <svg height={elHeight} width={props.width}>
-        <g transform={translate(margins.left, margins.top)}>
-          <defs>
-            <pattern id={`diagonalHatch-${id}`} patternUnits="userSpaceOnUse" width="4" height="4">
-              <path d="M-1,1 l2,-2 M0,4 l4,-4 M3,5 l2,-2" style={{stroke: 'black', strokeWidth: 1}} />
-            </pattern>
-            <clipPath id={`name-area-${id}`}>
-              <rect x={0} y={0} height={height} width={groupWidth} />
-            </clipPath>
-          </defs>
-          <g transform={translate(0, height)} ref={element => {
-            if (element) {
-              select(element).call(xAxisBottom);
-            }
-          }} />
-          <g className={cx({
-            group: true,
-            has_status: !!status
-          }, status)}>
-            {sortEntityByStartDate(data).map(([entity, links], index) => {
-              return (
-                <g key={entity.name} className={entity.name} transform={translate(0, yScale(index))}>
-                  <line className='group-separator' x1={0} x2={width} y1={0} y2={0} stroke='black' strokeOpacity={0.1} />
-                  <text className='entity-label' style={{clipPath: `url(#name-area-${id})`}} fontSize='1em' dy='1em' width={groupWidth}>
-                    {entity.name.length <= 22 ? entity.name : entity.name.slice(0,22)+'…'}
-                    <title>{entity.name}</title>
-                  </text>
-                  <g>
-                    {links.map((link: Link) => {
-                      const w = intervalRectWidth(link);
-                      return (
-                        <g key={link.id} transform={translate(xScale(link.start_year), intervalBarMargin)}>
-                          <rect
-                            rx='5'
-                            className={cx('link-rect', link.status.slug)}
-                            onClick={() => props.onLinkClick(link)}
-                            onMouseEnter={() => setHover({link: link, index: index})}
-                            fill={isNaN(w) ? `url(#diagonalHatch-${id})` : colorScale(link.status.slug)}
-                            stroke={hover && hover.link === link ? 'white' : 'black'}
-                            strokeOpacity={hover && hover.link === link ? 1 : 0.2}
-                            strokeWidth={hover && hover.link === link ? 2 : 1}
-                            width={isNaN(w) ? '100%' : w}
-                            height={intervalBarHeight}
-                            y={0}
-                            x={0}
-                          />
-                        </g>
-                      )
-                    })}
+      <div className="timeline">
+        <svg height={elHeight} width={props.width}>
+          <g transform={translate(margins.left, margins.top)}>
+            <defs>
+              <pattern id={`diagonalHatch-${id}`} patternUnits="userSpaceOnUse" width="4" height="4">
+                <path d="M-1,1 l2,-2 M0,4 l4,-4 M3,5 l2,-2" style={{stroke: 'black', strokeWidth: 1}} />
+              </pattern>
+              <clipPath id={`name-area-${id}`}>
+                <rect x={0} y={0} height={height} width={groupWidth} />
+              </clipPath>
+            </defs>
+            <g transform={translate(0, height)} ref={element => {
+              if (element) {
+                select(element).call(xAxisBottom);
+              }
+            }} />
+            <g className={cx({
+              group: true,
+              has_status: !!status
+            }, status)}>
+              {sortEntityByStartDate(data).map(([entity, links], index) => {
+                return (
+                  <g key={entity.name} className={entity.name} transform={translate(0, yScale(index))}>
+                    <line className='group-separator' x1={0} x2={width} y1={0} y2={0} stroke='black' strokeOpacity={0.1} />
+                    <text className='entity-label' style={{clipPath: `url(#name-area-${id})`, cursor: 'pointer'}} fontSize='1em' dy='1em' width={groupWidth} onClick={() => props.onEntityClick(entity.id)}>
+                      {entity.name.length <= 22 ? entity.name : entity.name.slice(0,22)+'…'}
+                        <title>{entity.name}</title>
+                    </text>
+                    <g>
+                      {links.map((link: Link) => {
+                        const w = intervalRectWidth(link);
+                        return (
+                          <g key={link.id} transform={translate(xScale(link.start_year), intervalBarMargin)}>
+                            <rect
+                              rx='5'
+                              className={cx('link-rect', link.status.slug)}
+                              onClick={() => props.onEntityClick(entity.id)}
+                              onMouseEnter={() => setHover({link: link, index: index})}
+                              fill={isNaN(w) ? `url(#diagonalHatch-${id})` : colorScale(link.status.slug)}
+                              stroke={hover && hover.link === link ? 'white' : 'black'}
+                              strokeOpacity={hover && hover.link === link ? 1 : 0.2}
+                              strokeWidth={hover && hover.link === link ? 2 : 1}
+                              width={isNaN(w) ? '100%' : w}
+                              height={intervalBarHeight}
+                              y={0}
+                              x={0}
+                            />
+                          </g>
+                        )
+                      })}
+                    </g>
                   </g>
-                </g>
-              )
-            })}
+                )
+              })}
+            </g>
           </g>
-        </g>
-      </svg>
+        </svg>
+        {hover && <div className='tooltip-container' style={{
+          transform: `translate(${xScale(hover.link.start_year)}px, ${yScale(hover.index)-props.lineHeight}px)`,
+          //minWidth: intervalRectWidth(hover.link)
+        }}>
+          <span className='tooltip'>{hover.link.GPH_name} - {hover.link.status.GPH_status} - {formater(hover.link.start_year)} to {formater(hover.link.end_year)}</span>
+        </div>}
+      </div>
     </div>
   );
 }
