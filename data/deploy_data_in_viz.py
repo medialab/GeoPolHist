713ff72169d2986b0e31def3822d1f1d1b280cba
Paul Girard <paul@ouestware.com>
2021-03-04 19:30:21 +0100

    rename _in_time _over_time

--- data/deploy_data_in_viz.py
+++ data/deploy_data_in_viz.py
@@ -4,13 +4,17 @@ import csv
 import json
 
 # copy full data
-print("copying GeoPolHist_entities_status_in_time.csv")
-copy("GeoPolHist_entities_status_in_time.csv", "../public/data/")
+print("copying GeoPolHist_entities_status_over_time.csv")
+copy("GeoPolHist_entities_status_over_time.csv", "../public/data/")
+# copy full data
+print("copying GeoPolHist_entities.csv")
+copy("GeoPolHist_entities.csv", "../public/data/")
 # copy status metadata
 print("copying GPH_status.json")
 with open('GeoPolHist_status.csv', 'r', encoding='utf8') as fcsv, open('../public/data/GPH_status.json', 'w', encoding='utf8') as fjson:
     status = csv.DictReader(fcsv)
-    json.dump({s['GPH_status']:{"GPH_status":s['GPH_status'], "slug":s['slug'], "priority":s['priority_order']} for s in status}, fjson, indent=2, ensure_ascii=False)    
+    json.dump({s['GPH_status']: {"GPH_status": s['GPH_status'], "slug": s['slug'],
+                                 "priority": s['priority_order']} for s in status}, fjson, indent=2, ensure_ascii=False)
 # copy Sankey data
 print("copying ./aggregated/status_transitions_links_periods.csv")
 copy("./aggregated/status_transitions_links_periods.csv", "../public/data/")
